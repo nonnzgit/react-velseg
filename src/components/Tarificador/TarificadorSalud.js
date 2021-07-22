ecf9c35d51a864504f07796e8fca87c4293303c4
Nonnz <jdvelrod@gmail.com>
2021-07-23 01:44:42 +0200

    1st tarif CRUD and styles implemented

--- src/components/Tarificador/TarificadorSalud.js
+++ src/components/Tarificador/TarificadorSalud.js
@@ -1,12 +1,67 @@
-import React from "react";
-import { ScTarificadorSalud } from "./style.js";
-import { ScSubtitle, Separator } from "../../globalStyles";
+import React, { useState } from "react";
+// ESTILOS
+import { ScTarificadorSalud, ScFechas, ScPanel, ScPrecio } from "./style.js";
+import { ScUserButtons } from "../Buttons/styles.js";
+import { ScSubtitle, Separator } from "../../globalStyles.js";
+// COMPONENTES
+import DatePick from "../Datepick/DatePick.js";
+// BOOTSTRAP
+import { Container, Row } from "react-bootstrap";
+// ICONS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserTimes } from "@fortawesome/free-solid-svg-icons";
+// UUID
+import { v4 as uuidv4 } from "uuid";
 
 const TarificadorSalud = () => {
+  const [fechaNac, setFechaNac] = useState([]);
+
+  const parseFechas = (fecha) => {
+    const dia = fecha.getDate() < 10 ? "0" + fecha.getDate() : fecha.getDate();
+    const mes =
+      fecha.getMonth() < 9
+        ? "0" + (fecha.getMonth() + 1)
+        : fecha.getMonth() + 1;
+    const anio = fecha.getFullYear();
+    return `${dia} / ${mes} / ${anio}`;
+  };
+
+  const handleFecha = (fechaSeleccionada) => {
+    setFechaNac([...fechaNac, { id: uuidv4(), Nac: fechaSeleccionada }]);
+  };
+
+  const borrarFecha = (id) => {
+    const filtrarFecha = fechaNac.filter((elem) => elem.id !== id);
+    setFechaNac([...filtrarFecha]);
+  };
+
   return (
     <ScTarificadorSalud>
-      <ScSubtitle responsive>Tarificador de Salud</ScSubtitle>
-      <Separator />
+      <Container fluid={"xl"}>
+        <ScSubtitle responsive>Tarificador de Salud</ScSubtitle>
+        <Separator />
+        <ScPanel>
+          <DatePick handleFecha={handleFecha} />
+          <ScFechas>
+            {fechaNac.map((elem, idx) => (
+              <Row key={elem.id} className="cardAsegurado">
+                <div className="datosAsegurado">
+                  <h5 className="numeroAsegurado">{`Asegurado ${idx + 1}`}</h5>
+                  <p className="fechaAsegurado">{parseFechas(elem.Nac)}</p>
+                </div>
+                <ScUserButtons
+                  danger
+                  dangerborder
+                  onClick={() => borrarFecha(elem.id)}
+                >
+                  <FontAwesomeIcon icon={faUserTimes}></FontAwesomeIcon>
+                </ScUserButtons>
+              </Row>
+            ))}
+          </ScFechas>
+          <ScPrecio />
+        </ScPanel>
+      </Container>
     </ScTarificadorSalud>
   );
 };
--- src/components/Tarificador/style.js
+++ src/components/Tarificador/style.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { colors } from "../../globalStyles";
 
 export const ScTarificadorSalud = styled.section`
   background: white;
@@ -9,3 +10,50 @@ export const ScTarificadorImpAlq = styled.section`
   background: white;
   padding-top: 5rem;
 `;
+
+export const ScPanel = styled.div`
+  padding: 5rem 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ScFechas = styled.div`
+  background: #d7ccc8;
+  border-radius: 10px;
+  padding: 3rem 1rem;
+  width: 18rem;
+  color: black;
+
+  .cardAsegurado {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .datosAsegurado {
+    width: 172.33px;
+    height: 48px;
+    margin: 16px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-bottom: 1.7px solid;
+
+    .numeroAsegurado {
+      color: rgba(0, 0, 0, 40%);
+      font-size: 0.8rem;
+      padding: 0;
+      margin-bottom: 7px;
+    }
+
+    .fechaAsegurado {
+      padding-bottom: 7px;
+      margin: 0;
+      line-height: 1rem;
+    }
+  }
+`;
+
+export const ScPrecio = styled.div``;
